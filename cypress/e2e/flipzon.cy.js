3b23b45fbe8b677991412db659b8f0604fce4698
Anon-Sonu <sonurprajapati1112@gmail.com>
2024-07-14 21:54:07 +0530

    new change

--- cypress/e2e/flipzon.cy.js
+++ cypress/e2e/flipzon.cy.js
@@ -1,46 +1,46 @@
+import { data } from '../fixtures/data.json';
 describe('My First Test', () => {
     var amazonPrice;
     it('Amazon test', () => {
-        cy.visit('https://www.amazon.in/')
-        cy.get("#twotabsearchtextbox").click().type("Let Us C Solutions").type("{enter}");
-        cy.contains("Let Us C Solutions -: Authenticate Solutions of Let US C Exercise (English Edition)").invoke('removeAttr', 'target').click();
-        cy.get(".a-price-whole").first().then($value => {
-            const textValue1 = $value.text();
-            amazonPrice = parseInt(textValue1);
+        cy.visit(data.amazon.visit)
+        cy.get(data.amazon.searchBox).click().type(data.amazon.staticTexts.letUsC).type(data.keyStrokes.enter);
+        cy.contains(data.amazon.staticTexts.letUsCSolutions).invoke('removeAttr', 'target').click();
+        cy.wait(10000)
+        cy.get(data.amazon.priceTag).first().then($value => {
+            amazonPrice = $value.text();
+            // cy.log(amazonPrice)
         })
 
         // cy.contains("Add to Cart").click();
         // cy.wait(5000);
         // cy.contains("Proceed to Buy").click({force: true});
         // cy.wait(5000);
+
     })
     var flipkartPrice;
     it('Flipkart test', () => {
-        cy.visit('https://www.flipkart.com/');
-        cy.get(".Pke_EE").click().type("Let Us C Solutions").type("{enter}");
-        cy.contains("Let Us C Solutions: Authenticate Solutions of Let US C").invoke('removeAttr', 'target').click();
-        // var a = cy.get(".hl05eU").invoke('text');
-        cy.get(".hl05eU").first().then($value => {
-            const textValue = $value.text();
-            const a = textValue.replace("₹", "");
-            flipkartPrice = parseInt(a);
+        cy.visit(data.flipkart.visit);
+        cy.get(data.flipkart.searchBox).click().type(data.amazon.staticTexts.letUsC).type(data.keyStrokes.enter);
+        cy.contains(data.flipkart.staticTexts.letUsCSolutions).invoke('removeAttr', 'target').click();
+        cy.get(data.flipkart.priceTag).first().then($value1 => {
+            const textValue1 = $value1.text();
+            flipkartPrice = textValue1.replace("₹", "");
+            // cy.log(flipkartPrice)
         })
 
-         // cy.contains("Add to cart").click({force: true});
+        // cy.contains("Add to cart").click({force: true});
         // cy.contains("Place Order").click({force: true});
         // cy.wait(5000);
-
-        
 
     })
     it('Final Result', () => {
         // cy.log("Amazon: " + amazonPrice);
         // cy.log("Flipkart: " + flipkartPrice);
-        if(amazonPrice<flipkartPrice){
-            cy.log("Amazone is better")
+        if (parseInt(amazonPrice) > parseInt(flipkartPrice)) {
+            cy.log("Amazone is cheaper")
         }
-        else{
-            cy.log("Flipkart is better")
+        else {
+            cy.log("Flipkart is cheaper")
         }
     })
-  })
+})
